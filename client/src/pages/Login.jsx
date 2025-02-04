11532080a013a706f0d9a9d93e271c764c6c9b2f
Receipes App <student.siddhanthchapade@gmail.com>
2025-02-04 23:36:46 +0530

    Login Page Inprogress

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -16,20 +16,48 @@ useEffect(()=>{
     user && navigate("/dashboard");
 },[user]);
 
+const submitHandler = async (data) =>{
+  console.log("submit");
+}
 
 
 return (<>
 
-     <div className='w-full min-h-screen flex items-center -justify-center flex-col lg:flex-row bg-[#f3f4f6]'></div>
+     <div className='w-full min-h-screen flex items-center -justify-center flex-col lg:flex-row bg-[#f3f4f6]'>
         <div className='w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center'>
-
-             <div className='h-full w-full lg:w-2/3 flex flex-col items-center jsutify-center'>
+            {/*left side*/}
+             <div className='h-full w-full lg:w-2/3 flex flex-col items-center justify-center'>
              <div className='w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-2xl:-mt-20 '>
-                <span className='flex gap-1 py-1 px-3 border rounded-full text-xl md:text-base  text-black'>Manage all your tasks here</span>
+                <span className='flex gap-1 py-1 px-3 border rounded-full border-blue-200 text-xl md:text-base  text-black'>Manage all your tasks here</span>
+
+                <p className='flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black
+                text-center text-blue-700'>
+                  <span>Cloud-Based</span>
+                  <span>Task Manager</span>
+                </p>
+              <div className='cell'>
+                <div className='circle rotate-in-up-left'></div>
+              </div>
+            </div>
              </div>
-             
+
+             {/*right side*/}
+             <div className='w-full md:w-1/3 p-4 md:p-1 flex flex-col justify-center items-center'>
+               <form onSubmit={handleSubmit(submitHandler)}
+               className='form-container w-full md:w-[400px] flex flex-col gap-y-8 bg-white  px-10 pt-14 pb-14'>
+                <div className=''>
+                  <p className='text-blue-600 text-3xl font-bold text-center'>
+                    Welcome Back!
+                  </p>
+                  <span>Keep all your crediential safe</span>
+                </div>
+
+
+               </form>
+
              </div>
             
+        </div>
         </div>
   
   
